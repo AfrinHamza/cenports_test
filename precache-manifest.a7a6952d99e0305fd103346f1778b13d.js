e4357d5bdad1d53fa42a8cfd0996c06dad42bf07
AfrinHamza <afrin@vezolve.com>
2022-06-10 10:12:39 +0530

    Updates

---
+++ precache-manifest.a7a6952d99e0305fd103346f1778b13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16fe6809193e609f8777",
+    "url": "/my-app-cenports/static/css/main.34f7f3e1.chunk.css"
+  },
+  {
+    "revision": "16fe6809193e609f8777",
+    "url": "/my-app-cenports/static/js/main.c6f8bd6f.chunk.js"
+  },
+  {
+    "revision": "31b62d06959ee20eb03c",
+    "url": "/my-app-cenports/static/js/runtime~main.833d5683.js"
+  },
+  {
+    "revision": "17fcdaead07bd49d288e",
+    "url": "/my-app-cenports/static/js/2.3edc95a4.chunk.js"
+  },
+  {
+    "revision": "86e367d0e916747d12b2",
+    "url": "/my-app-cenports/static/js/3.b3cfcba4.chunk.js"
+  },
+  {
+    "revision": "f49dea0bb06ffb6d54fea85c47c02290",
+    "url": "/my-app-cenports/index.html"
+  }
+];
