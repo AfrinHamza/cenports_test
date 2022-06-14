cfc2582ee2a9316cfcaf5cd09a3b578fc3c039f4
AfrinHamza <afrin@vezolve.com>
2022-06-14 13:52:12 +0530

    Updates

---
+++ precache-manifest.ae37d0676449f86e2cb9fc1595078d02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9029580f8d9eb1eb0849",
+    "url": "/my-app-cenports/static/css/main.64ddfc05.chunk.css"
+  },
+  {
+    "revision": "9029580f8d9eb1eb0849",
+    "url": "/my-app-cenports/static/js/main.9029580f.chunk.js"
+  },
+  {
+    "revision": "17a3d24873f6441f0ccd",
+    "url": "/my-app-cenports/static/js/1.17a3d248.chunk.js"
+  },
+  {
+    "revision": "4cea1cd18cfda3e4079a",
+    "url": "/my-app-cenports/static/js/2.4cea1cd1.chunk.js"
+  },
+  {
+    "revision": "65b147f79de5be47f301",
+    "url": "/my-app-cenports/static/js/runtime~main.65b147f7.js"
+  },
+  {
+    "revision": "c88ca3a34a85ee8ce9f07e1dc27ac8a8",
+    "url": "/my-app-cenports/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app-cenports/precache-manifest.78f49fdfd9d6551e70dcf2b192cd1e4f.js"
+  "/my-app-cenports/precache-manifest.ae37d0676449f86e2cb9fc1595078d02.js"
 );
 
 workbox.clientsClaim();
